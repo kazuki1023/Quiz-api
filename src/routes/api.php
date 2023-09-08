b76dde7c058ee7fcb6c2c0b70900aa8785fc3302
kazuki1023 <k.keio12569@gmail.com>
2023-09-08 14:23:16 +0900

    api の定義終了

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::prefix('/v1')->group(function () {
+    Route::apiResource('/quiz', QuizController::class);
+    Route::get('/', [QuizController::class, 'index']);
 });
