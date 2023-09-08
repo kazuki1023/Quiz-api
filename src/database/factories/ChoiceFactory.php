f8a81371cc68c360560dfeb4daef6edfe47ffc52
kazuki1023 <k.keio12569@gmail.com>
2023-09-08 14:58:16 +0900

    add factory

---
+++ src/database/factories/ChoiceFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Quiz;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Choice>
+ */
+class ChoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Choice::class;
+    public function definition(): array
+    {
+        return [
+            'quiz_id' => Quiz::factory(),
+            'answer' => $this->faker->word,
+            'valid' => $this->faker->numberBetween(0, 1),
+        ];
+    }
+}
---
+++ src/database/factories/QuizFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Quiz>
+ */
+class QuizFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Quiz::class;
+    public function definition(): array
+    {
+        return [
+            'content' => $this->faker->sentence,
+            'img' => $this->faker->imageUrl(),
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+    }
+}
